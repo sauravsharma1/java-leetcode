7a58b4a27116c93ccd3443dd733858236d5f8b21
sauravsharma1 <56475622+sauravsharma1@users.noreply.github.com>
2022-04-27 03:02:29 +0530

    Time: 155 ms (77.19%), Space: 67.5 MB (80.33%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+        PriorityQueue<int[]> pq=new PriorityQueue<int[]>((a,b)->a[2]-b[2]);
+        
+        int cost=0;
+        
+        pq.offer(new int[]{0,0,0});
+        
+        Set<Integer> visited=new HashSet<Integer>();
+        
+        int num=points.length;
+        
+        while(!pq.isEmpty()&&visited.size()<num){
+            
+            int curr[]=pq.poll();
+            
+            int endId=curr[1];
+            
+            int currCost=curr[2];
+            
+            if(visited.contains(curr[1])){
+                continue;
+            }
+            
+            cost+=currCost;
+            
+            visited.add(curr[1]);
+            
+            for(int i=0;i<num;i++){
+                
+                if(!visited.contains(i)){
+                    pq.offer(new int[]{endId,i,distance(points,endId,i)});
+                }
+                
+            }
+            
+            
+            
+        }
+        return cost;
+    }
+    
+    private int distance(int [][] points,int i,int j){
+        
+        return Math.abs(points[i][0]-points[j][0])+Math.abs(points[i][1]-points[j][1]);
+        
+    }
+    
+}
