f2d8030ee0c0642e0aa714d54cada501ca72457c
sauravsharma1 <56475622+sauravsharma1@users.noreply.github.com>
2022-06-03 04:53:42 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (27.25%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if(m==0||n==0){
+            throw new IllegalArgumentException("cant be zero");
+        }
+        
+        int[][] memo=new int[m][n];
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                memo[i][j]=-1;
+            }
+        }
+        
+        return numPaths(m-1,n-1,memo);
+        
+        
+        
+    }
+    
+    
+    private int numPaths(int i,int j,int[][] memo){
+        
+        if(i<0||j<0){
+            return 0;
+        }
+        
+        if(i==0||j==0){
+            return 1;
+        }
+        
+        if(memo[i][j]!=-1)
+        {
+            return memo[i][j];
+        }        
+        
+        return memo[i][j]=numPaths(i-1,j,memo)+numPaths(i,j-1,memo);
+        
+    }
+    
+}
